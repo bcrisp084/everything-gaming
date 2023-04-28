f1d2aa564a977a3e4da40d769573e882f06f09e0
Brian Crisp <bcrisp084@yahoo.com>
2023-04-27 23:30:06 -0400

    Changed the langing page image/login buttons

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,38 +1,37 @@
-import homePhoto from "../assets/home-photo.jpg";
+import Nightgame from "../assets/Nightgame.jpg";
 import Image from "next/image";
 
 const IndexPage = () => (
-  <main className="md:container md:mx-auto">
-    <div className="container h-screen">
-      <nav className="bg-black-950 shadow opacity-90 rounded">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex justify-between h-16">
-            <div className="flex-shrink-0 flex items-center">
-              <h1 className="text-white text-xl">Everything Gaming</h1>
-            </div>
-            <div className="flex items-center">
-              <a
-                href="/login"
-                id="login-button"
-                className="text-white-600 text-xl hover:opacity-80  px-3 py-2 rounded-md  font-medium"
-              >
-                Login
-              </a>
-              <a
-                href="/signup"
-                id="sign-up-button"
-                className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 text-md font-medium ml-3"
-              >
-                Sign up
-              </a>
-            </div>
+  <>
+    <main className="home-screen">
+      <Image className="home-screen-image" src={Nightgame} alt="Home" />
+    </main>
+    <nav className="z-1 bg-black-950 shadow opacity-90 rounded">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16">
+          <div className="flex-shrink-0 flex items-center">
+            <h1 className="text-white text-5xl">Everything Gaming</h1>
+          </div>
+          <div className="flex items-center">
+            <a
+              href="/login"
+              id="login-button"
+              className="text-white text-xl hover:opacity-80  px-3 py-2 rounded-md  font-medium"
+            >
+              Login
+            </a>
+            <a
+              href="/signup"
+              id="sign-up-button"
+              className="bg-black text-white px-4 py-2 rounded-md hover:opacity-80 text-md font-medium ml-3"
+            >
+              Sign up
+            </a>
           </div>
         </div>
-      </nav>
-      <Image className="h-4/5 object-cover" src={homePhoto} alt="Home" />
-    </div>
-    <div className="container h-screen"></div>
-  </main>
+      </div>
+    </nav>
+  </>
 );
 
 export default IndexPage;
