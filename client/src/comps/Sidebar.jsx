d22e78853c797668b041ea04b01118ec5f53d360
Brian Crisp <bcrisp084@yahoo.com>
2023-04-27 23:57:59 -0400

    changed sidebar link colors to purple

--- client/src/comps/Sidebar.jsx
+++ client/src/comps/Sidebar.jsx
@@ -6,44 +6,117 @@ export default function Sidebar() {
       <div className="sidebar__item">
         <span className="sidebar__title text-3xl">username</span>
         <ul>
-          <li>Wishlist</li>
-          <li>My games</li>
-          <li>My reviews</li>
+          <li>
+            <Link className="side-link" href="/wishlist">
+              Wishlist
+            </Link>
+          </li>
+          <li>
+            <Link className="side-link" href="/mygame">
+              My games
+            </Link>{" "}
+          </li>
+          <li>
+            <Link className="side-link" href="/reviews">
+              My reviews
+            </Link>{" "}
+          </li>
         </ul>
       </div>
       <div className="sidebar__item">
         <span className="sidebar__title text-3xl">Genres</span>
         <ul>
-          <li>Action</li>
-          <li>Adventure</li>
-          <li>Indie</li>
-          <li>RPG</li>
+          <li>
+            <Link className="side-link" href="/action">
+              Action
+            </Link>{" "}
+          </li>
+          <li>
+            <Link className="side-link" href="/adventure">
+              Adventure
+            </Link>{" "}
+          </li>
+          <li>
+            <Link className="side-link" href="/indie">
+              Indie
+            </Link>{" "}
+          </li>
+          <li>
+            <Link className="side-link" href="/rpg">
+              RPG
+            </Link>{" "}
+          </li>
         </ul>
       </div>
       <div className="sidebar__item">
         <span className="sidebar__title text-3xl">Platforms</span>
         <ul>
-          <li>PC</li>
-          <li>Playstation</li>
-          <li>Xbox</li>
-          <li>Nintendo</li>
+          <li>
+            <Link className="side-link" href="/pc">
+              PC
+            </Link>{" "}
+          </li>
+          <li>
+            <Link className="side-link" href="/playstation">
+              Playstation
+            </Link>{" "}
+          </li>
+          <li>
+            <Link className="side-link" href="/xbox">
+              Xbox
+            </Link>{" "}
+          </li>
+          <li>
+            <Link className="side-link" href="/nintendo">
+              Nintendo
+            </Link>{" "}
+          </li>
         </ul>
       </div>
       <div className="sidebar__item">
-        <span className="sidebar__title text-3xl">Other</span>
+        <span className="sidebar__title text-3xl">Mode</span>
         <ul>
-          <li>Free to play</li>
-          <li>Multiplayer</li>
-          <li>Singleplayer</li>
-          <li>Co-op</li>
+          <li>
+            <Link className="side-link" href="/free">
+              Free to play
+            </Link>{" "}
+          </li>
+          <li>
+            <Link className="side-link" href="/multiplayer">
+              Multiplayer
+            </Link>{" "}
+          </li>
+          <li>
+            <Link className="side-link" href="/singleplayer">
+              Singleplayer
+            </Link>{" "}
+          </li>
+          <li>
+            <Link className="side-link" href="/coop">
+              Co-op
+            </Link>{" "}
+          </li>
         </ul>
       </div>
       <div className="sidebar__item">
         <span className="sidebar__title text-3xl">Voted</span>
         <ul>
-          <li>Best of 2022</li>
-          <li>Greatest Hits</li>
-          <li>Top 50</li>
+          <li>
+            <Link className="side-link" href="/bestof2022">
+              Best of 2022
+            </Link>{" "}
+          </li>
+          <li>
+            {" "}
+            <Link className="side-link" href="greatesthits">
+              Greatest Hits
+            </Link>{" "}
+          </li>
+          <li>
+            <Link className="side-link" href="top50">
+              Top 50
+            </Link>{" "}
+          </li>
         </ul>
       </div>
     </div>
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -10,7 +10,9 @@ const IndexPage = () => (
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex-shrink-0 flex items-center">
-            <h1 className="text-white text-5xl">Everything Gaming</h1>
+            <h1 className="text-white text-5xl brand-name">
+              Everything Gaming
+            </h1>
           </div>
           <div className="flex items-center">
             <a
