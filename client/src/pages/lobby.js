67b4de5646f4d9b0a2e51c0934877800d3dad318
Brian Crisp <bcrisp084@yahoo.com>
2023-09-24 00:16:47 -0400

    .

--- client/src/pages/lobby.js
+++ client/src/pages/lobby.js
@@ -50,36 +50,35 @@ function Lobby() {
 
   return (
     <Container>
-       <Nav />
-       <div className="h-full w-150 fixed z-1 top-0 left-0  overflow-x-hidden pt-20 bg-black">
-         <Sidebar />
-       </div>
-     <div className="ml-[150px]">      
-      <Grid.Container gap={2} justify="center">
-        {games.map((game, index) => (
-          <Grid xs={12} md={6} lg={4} key={game.id}>
-            <Link href={`/${game.id}`}>
-            <GameCard
-              shadow="true"
-              hoverable="true"
-              width="100%"
-              src={game.background_image}
-              name={game.name}
-              released={game.released}
-            />
-            </Link>
-            {games.length === index + 1 && (
-              <div ref={lastGameRef} className="game">
-                {/* this element will be observed by the IntersectionObserver */}
-              </div>
-            )}
-          </Grid>
-        ))}
-      </Grid.Container>
+      <Nav />
+      <div className="h-full w-150 fixed z-1 top-0 left-0  overflow-x-hidden pt-20 bg-black">
+        <Sidebar />
+      </div>
+      <div className="ml-[150px]">
+        <Grid.Container gap={2} justify="center">
+          {games.map((game, index) => (
+            <Grid xs={12} md={6} lg={4} key={game.id}>
+              <Link href={`/${game.id}`}>
+                <GameCard
+                  shadow="true"
+                  hoverable="true"
+                  width="100%"
+                  src={game.background_image}
+                  name={game.name}
+                  released={game.released}
+                />
+              </Link>
+              {games.length === index + 1 && (
+                <div ref={lastGameRef} className="game">
+                  {/* this element will be observed by the IntersectionObserver */}
+                </div>
+              )}
+            </Grid>
+          ))}
+        </Grid.Container>
       </div>
     </Container>
   );
 }
 
 export default Lobby;
-
